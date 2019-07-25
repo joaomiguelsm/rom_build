1ec996f3f1de89ad0a9f11f03741c4d505658090
joaomiguelsm <joaomiguel0100@gmail.com>
2019-07-24 21:43:42 -0300

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir .repo/local_manifests
-curl --create-dirs -L -o .repo/local_manifests/xiaomi_msm8996_default.xml -O -L https://raw.github.com/joaomiguelsm/android_.repo_local_manifest/pe-pie/xiaomi_msm8996_default.xml >/dev/null  2>&1
+curl --create-dirs -L -o .repo/local_manifests/xiaomi_msm8996_default.xml -O -L https://raw.github.com/JonasCardoso/android_.repo_local_manifest/pe-pie/xiaomi_msm8996_default.xml >/dev/null  2>&1

43dc5df26353e0f407c8c9d5524db03b30310525
joaomiguelsm <joaomiguel0100@gmail.com>
2019-07-24 21:24:46 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-GITHUB_USER=JonasCardoso
-GITHUB_EMAIL=jonascard60@gmail.com
+GITHUB_USER=joaomiguelsm
+GITHUB_EMAIL=joaomiguel0100@gmail.com
 
-KBUILD_BUILD_USER="jonas"
-KBUILD_BUILD_HOST="floppy"
+KBUILD_BUILD_USER="joaomattos"
+KBUILD_BUILD_HOST="drinker"
 
 ROM="PixelExperience"
 manifest_url="https://github.com/PixelExperience/manifest"
 export rom_vendor_name="aosp" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
 branch="pie"
 
-release_repo="JonasCardoso/rom_build"
+release_repo="joaomiguelsm/rom_build"
 
 timezone="America/Sao_Paulo"

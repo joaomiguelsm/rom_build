ba87ea1dc97bcde29059330464f42afc78066d22
joaomiguelsm <joaomiguel0100@gmail.com>
2019-07-29 22:10:20 -0300

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir .repo/local_manifests
-curl --create-dirs -L -o .repo/local_manifests/xiaomi_sdm660_default.xml -O -L https://raw.github.com/joaomiguelsm/jasmine_sprout_build_manifest >/dev/null  2>&1
+curl --create-dirs -L -o .repo/local_manifests/xiaomi_sdm660_default.xml -O -L https://raw.githubusercontent.com/joaomiguelsm/jasmine_sprout_build_manifest/aosp/device_manifest.xml >/dev/null  2>&1
